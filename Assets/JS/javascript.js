10d85b911a8316ee29c77675cfd3d28c193e7954
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2020-12-10 17:52:56 +1100

    js pseudoCode

---
+++ Assets/JS/javascript.js
@@ -0,0 +1,187 @@
+// Create array of objects for questions & answers
+var questions = [
+    {
+        title: "1) Which 1980 Disney film is about an aquatic creature who dreams of becoming human after falling in love with a prince?",
+        choices: [
+            "Fantasia",
+            "The Little Mermaid",
+            "Peter Pan",
+            "Cinderella"
+        ],
+        //Answer: B
+        answer: "The Little Mermaid",
+    },
+    {
+        title: "2) Which 1984 Hayao Miyazaki film about a polluted world is attributed to cementing Miyazaki's reputation as an animator?",
+        choices: [
+            "Ocean Waves",
+            "Castle in the Sky",
+            "Porco Rosso",
+            "Nausicaä of the Valley of the Wind"
+        ],
+        // Answer: D
+        answer: "Nausicaä of the Valley of the Wind",
+    },
+    {
+        title: "3) Which Ubisoft video game series follows an organization with a doctrine that says 'Nothing is Real; Everything is Permitted' and 'We work in the dark to serve the light'?",
+        choices: [
+            "Assassin's Creed",
+            "Call of Duty",
+            "Borderlands",
+            "Fallout",
+        ],
+        // Answer: A
+        answer: "Assassin's Creed",
+    },
+    {
+        title: "4) Released in 1991, which Disney fairy tale set in France follows a bookworm who falls in love with a cursed prince?",
+        choices: [
+            "The Hunchback of Notre Dame",
+            "Tarzan",
+            "Beauty and the Beast",
+            "Shrek"
+        ],
+        // Answer: C
+        answer: "Beauty and the Beast",
+    },
+    {
+        title: "5) Which 1988 Studio Ghibli film follows a pair of young siblings struggling to survive during the final months of World War II?",
+        choices: [
+            "When Marnie was There",
+            "Arrietty",
+            "Only Yesterday",
+            "Grave of the Fireflies"
+        ],
+        // Answer: D
+        answer: "Grave of the Fireflies",
+    },
+    {
+        title: "6) Set in the fantasy world of Spira, which Square Enix video game follows a group of adventurers on their quest to defeat a rampaging monster known as Sin?",
+        choices: [
+            "Final Fantasy",
+            "Final Fantasy V",
+            "Final Fantasy X",
+            "Final Fantasy XV"
+        ],
+        // Answser: C
+        answer: "Final Fantasy X",
+    },
+    {
+        title: "7) Which 1992 Disney film folows a street rat and his pet monkey as they try to thwart the evil plans of a Royal Vizier by enlisting the help of a genie and magic carpet?",
+        choices: [
+            "Aladdin",
+            "Bambi",
+            "Dumbo",
+            "The Jungle Book"
+        ],
+        // Answer: A1
+        answer: "Aladdin",
+    },
+    {
+        title: "8) GHIBLI?",
+        choices: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        answer: "",
+    },
+    {
+        title: "9) GAME?",
+        choices: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        answer: "",
+    },
+    {
+        title: "10) DISNEY?",
+        choices: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        answer: "",
+    },
+    {
+        title: "11) GHIBLI?",
+        choices: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        answer: "",
+    },
+    {
+        title: "12) GAME?",
+        choices: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        answer: "",
+    },
+    {
+        title: "13) DISNEY?",
+        choices: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        answer: "",
+    },
+    {
+        title: "14) GHIBLI?",
+        choices: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        answer: "",
+    },
+    {
+        title: "15) GAME?",
+        choices: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        answer: "",
+    },
+]
+
+// Declare variables for score & timer
+
+// "click" eventListener to trigger timer & quiz
+
+// Render questions to page
+
+// forLoop to generate questions
+
+// Action next questions
+
+// If, Else function to compare user selection with answer
+
+    // If correct, add score & display msg
+
+    // If incorrect, deduct score, deduct time & display msg
+
+// Display results & score
+// User input of initials for Leaderboard
+    // Set input parameters (min 3 characters, max 4)
+
+// Submit button w/"click" eventListener
+    
+    // Store results on Local Storage
+
+// Display Leaderboard
+    // Add quizScore to leaderBoard
---
+++ Assets/JS/leaderboard.js
@@ -0,0 +1,7 @@
+// Declare variables for score & timer
+
+// "click" eventListener to restart quiz (go Home)
+
+// "click" eventListener to clear Leaderboard
+
+// Create Leaderboard from local storage data; sort by high score
